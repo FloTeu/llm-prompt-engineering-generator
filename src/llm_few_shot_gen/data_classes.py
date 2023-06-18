ddd37b119424f31fe8a4f2a4bd7224b56a760803
fteutsch <florian.teutsch@inovex.de>
2023-06-18 12:52:16 +0200

    change message template

--- src/llm_few_shot_gen/data_classes.py
+++ src/llm_few_shot_gen/data_classes.py
@@ -6,8 +6,8 @@ from langchain.prompts.chat import BaseMessagePromptTemplate
 @dataclass
 class FewShotGenerationMessages:
     instruction_message: Optional[SystemMessagePromptTemplate] = None
-    context: Optional[List[BaseMessagePromptTemplate]] = None
-    few_shot_examples: Optional[List[BaseMessagePromptTemplate]] = None
+    context: Optional[List[SystemMessagePromptTemplate]] = None
+    few_shot_examples: Optional[List[SystemMessagePromptTemplate]] = None
     human_message: Optional[HumanMessagePromptTemplate] = None
 
     def is_instruction_known(self):
