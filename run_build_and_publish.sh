958436a38318c4deb25d879b1527311cf2a45a68
fteutsch <florian.teutsch@inovex.de>
2023-06-25 18:17:42 +0200

    Add script to automatic version update build and publishing of package

---
+++ run_build_and_publish.sh
@@ -0,0 +1,16 @@
+read -p "Have you already updated the version? If no, patch version update will be performed (yes/no): " response
+
+if [[ "$response" == "yes" ]]; then
+    # Your code here
+    echo "Build package..."
+    hatch build
+    echo "Publish package..."
+    #hatch publish
+else
+    echo "Update version..."
+    hatch version patch
+    echo "Build package..."
+    hatch build
+    echo "Publish package..."
+    #hatch publish
+fi
